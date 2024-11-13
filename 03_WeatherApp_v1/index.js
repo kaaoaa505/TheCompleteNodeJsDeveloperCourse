21afb3412f71951cf7ed21e1807ac4924de1e3dc
Khaled Allam <kaaoaa505@gmail.com>
2024-11-13 11:05:31 +0300

    process weather search by address or lat&lng in callback methods

--- 03_WeatherApp_v1/index.js
+++ 03_WeatherApp_v1/index.js
@@ -8,45 +8,13 @@ const url = 'http://api.weatherstack.com/current?access_key=6e437138879d3336efe3
 request({ url: url, json: true }, function (error, response, body) {
   try {
     console.log('error:', error);
-
-    console.log(`
-    ________________________
-    `);
-
     console.log('statusCode:', response && response.statusCode);
-
     console.log(`
     ________________________
     `);
 
-    //   console.log('body:', body);
+    console.log('data.current:', body.current);
 
-    // const data = JSON.parse(body);
-    const data = body;
-
-    console.log('data:', data);
-    
-    console.log(`
-    ________________________
-    `);
-
-    console.log('data.current:', data.current);
-    
-    console.log(`
-    ________________________
-    `);
-    
-    console.log('data.current.temperature:', data.current.temperature);
-    
-    console.log(`
-    ________________________
-    `);
-    
-    console.log('data.current.weather_descriptions[0]:', data.current.weather_descriptions[0]);
-    
-    console.log(`
-    ________________________
-    `);
 
   } catch (errorTryCatch) {
     console.log('errorTryCatch:', errorTryCatch);
