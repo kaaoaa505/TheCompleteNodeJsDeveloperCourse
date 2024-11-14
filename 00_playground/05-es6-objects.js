49d68a59557cab61f7493714711d8bace7fe1c44
Khaled Allam <kaaoaa505@gmail.com>
2024-11-14 09:40:11 +0300

    Object property shorthand

---
+++ 00_playground/05-es6-objects.js
@@ -0,0 +1,12 @@
+// Object property shorthand
+const name = 'Khaled';
+const age = 35;
+const location = 'Saudi Arabia';
+
+const user = {
+    name,
+    age,
+    location,
+};
+
+console.log(user);
